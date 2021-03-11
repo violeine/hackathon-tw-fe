a6c020329fe4712501f0d11d0a0ac4a811d42bd6
violeine <haluudang@gmail.com>
2021-03-11 22:03:51 +0700

    basic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useState, useEffect } from "react";
+import { useChat } from "./hooks";
+import "./index.css";
 
 function App() {
+  const [room, setRoom] = useState();
+  const [input, setInput] = useState();
+  const [username, setUsername] = useState();
+  useEffect(() => {
+    console.log(window.location.pathname);
+    setUsername(localStorage.get("username"));
+    const room = window.location.pathname.substr(1);
+    if (room != "") {
+      setRoom(room);
+    }
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-red-50 w-full h-screen p-1">
+      {!room && (
+        <div className=" w-1/2 pt-6 mt-6 flex items-center mx-auto">
+          <input
+            className="w-64 rounded block shadow-lg p-4 mr-2"
+            type="number"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <input
+            className="w-64 rounded block shadow-lg p-4 mr-2"
+            type="number"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <a
+            className="bg-green-500 px-2 py-2 shadow-lg inline-block rounded-lg"
+            onClick={() => {
+              setRoom(input);
+            }}
+          >
+            Join
+          </a>
+        </div>
+      )}
+      {room && <Chat roomId={room} />}
     </div>
   );
 }
 
+function Chat({ roomId }) {
+  const { messages, sendMessage } = useChat(roomId);
+  const [input, setInput] = useState();
+  return (
+    <div className="bg-green-50 w-96 h-96 mt-6 ml-6">
+      <div className="h-80 bg-red-100">
+        {messages &&
+          messages.map(({ body, ownedByCurrentUser }) => (
+            <p className={`w-6 ${ownedByCurrentUser ? "ml-auto" : "mr-auto"}`}>
+              {body}
+            </p>
+          ))}
+      </div>
+      <div className="flex items-center">
+        <input
+          type="text"
+          className="w-full mr-2 mt-2 p-3"
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+        />
+        <a
+          className="bg-green-500 px-2 py-2 shadow-lg inline-block rounded-lg"
+          onClick={() => {
+            sendMessage(input);
+          }}
+        >
+          send
+        </a>
+      </div>
+    </div>
+  );
+}
 export default App;
---
+++ src/hooks.js
@@ -0,0 +1,40 @@
+import { useEffect, useRef, useState } from "react";
+import socketIOClient from "socket.io-client";
+
+const NEW_CHAT_MESSAGE_EVENT = "newChatMessage";
+const SOCKET_SERVER_URL = "localhost:4000";
+
+export const useChat = (roomId) => {
+  const [messages, setMessages] = useState([]);
+  const socketRef = useRef();
+
+  useEffect(() => {
+    socketRef.current = socketIOClient(SOCKET_SERVER_URL, {
+      query: { roomId },
+    });
+
+    socketRef.current.on(NEW_CHAT_MESSAGE_EVENT, (message) => {
+      const incomingMessage = {
+        ...message,
+        ownedByCurrentUser: message.senderId === socketRef.current.id,
+      };
+      setMessages((messages) => [...messages, incomingMessage]);
+    });
+
+    return () => {
+      socketRef.current.disconnect();
+    };
+  }, [roomId]);
+
+  const sendMessage = (messageBody,userName) => {
+    socketRef.current.emit(NEW_CHAT_MESSAGE_EVENT, {
+      user: userName,
+      body: messageBody,
+      senderId: socketRef.current.id,
+    });
+  };
+
+  return { messages, sendMessage };
+};
+
+
